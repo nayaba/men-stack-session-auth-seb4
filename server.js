77e4b96a0a03d1b92311b311e43c30ba56c41a4c
Nabila Ayaba <nabilaayaba@gmail.com>
2025-02-02 14:36:50 +0300

    Set up server

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+// const dotenv = require('dotenv')
+// dotenv.config()
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+
+const port = process.env.PORT ? process.env.PORT : '3000'
+
+// creates a connection to MONGO database
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}`)
+})
