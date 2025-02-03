bb79f0e754937f5f8a69af4eb6565eefd7522cbe
Nabila Ayaba <nabilaayaba@gmail.com>
2025-02-03 10:42:26 +0300

    Add bootstrap navbar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 const signUp = (req, res) => {
@@ -14,9 +15,18 @@ const addUser = async (req, res) => {
             msg: 'Username already taken.'
         })
     }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.render('auth/sign-up.ejs', {
+            title: 'Sign up',
+            msg: 'Password and Confirm Password must match.'
+        })
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+
     const user = await User.create(req.body)
     console.log('new user: ', user)
-
+    return res.send(`Thanks for signing up ${user.username}`)
 }
 
 module.exports = {
