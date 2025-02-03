6636bc0c96425bec7ff8c07a4504fdd3d0b786c3
Nabila Ayaba <nabilaayaba@gmail.com>
2025-02-03 10:02:15 +0300

    Add password/username message

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,22 @@
+const User = require('../models/user')
+
 const signUp = (req, res) => {
-    res.render('auth/sign-up.ejs', {title: 'Sign up'} )
+    res.render('auth/sign-up.ejs', 
+        {title: 'Sign up', msg: ''} )
 }
 
-const addUser = (req, res) => {
+const addUser = async (req, res) => {
     console.log('request body: ', req.body)
+    const userInDatabase = await User.findOne({ username: req.body.username})
+    if (userInDatabase) {
+        return res.render('auth/sign-up.ejs',{
+            title: 'Sign up', 
+            msg: 'Username already taken.'
+        })
+    }
+    const user = await User.create(req.body)
+    console.log('new user: ', user)
+
 }
 
 module.exports = {
